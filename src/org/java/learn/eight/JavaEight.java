67f2a873f3d74f9776d7c2c5209a0b97cc134a77
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2023-03-18 22:52:25 +0600

    -loop

--- src/org/java/learn/eight/JavaEight.java
+++ src/org/java/learn/eight/JavaEight.java
@@ -3,7 +3,9 @@ package org.java.learn.eight;
 public class JavaEight {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("print -> " + i);
+		}
 	}
 
 }
