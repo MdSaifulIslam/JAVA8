e392f77ee9a564cead156cdffad386ddae0ceb12
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2023-03-18 23:01:17 +0600

    git commit -m "Add bin folder to .gitignore"

--- src/org/java/learn/eight/JavaEight.java
+++ src/org/java/learn/eight/JavaEight.java
@@ -1,11 +1,11 @@
 package org.java.learn.eight;
 
 public class JavaEight {
-	
+
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			System.out.println("print -> " + i);
-		}
+		String[] numbers = { "one", "two", "three", "four", "five" };
+		
+		numbers.
 	}
 
 }
